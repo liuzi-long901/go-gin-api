e1ecc291832d170dee4ebe453e845155697102ef
刘子龙 <liuzl@suosi.com.cn>
2022-09-12 23:02:08 +0800

    update

--- global/app.go
+++ global/app.go
@@ -2,12 +2,16 @@ package global
 
 import (
 	"github.com/spf13/viper"
+	"go.uber.org/zap"
+	"gorm.io/gorm"
 	"jassue-gin/config"
 )
 
 type Application struct {
 	ConfigViper *viper.Viper
 	Config      config.Configuration
+	Log         *zap.Logger
+	DB          *gorm.DB
 }
 
 var App = new(Application)
--- main.go
+++ main.go
@@ -12,6 +12,20 @@ func main() {
 	bootstrap.InitializeConfig()
 	r := gin.Default()
 
+	// 初始化日志
+	global.App.Log = bootstrap.InitializeLog()
+	global.App.Log.Info("log init success!")
+
+	// 初始化数据库
+	global.App.DB = bootstrap.InitializeDB()
+	// 程序关闭前，释放数据库连接
+	defer func() {
+		if global.App.DB != nil {
+			db, _ := global.App.DB.DB()
+			db.Close()
+		}
+	}()
+
 	// 测试路由
 	r.GET("/ping", func(c *gin.Context) {
 		c.String(http.StatusOK, "pong")
