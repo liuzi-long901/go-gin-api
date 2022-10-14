7938847de633e56311c485d82f282f633142228a
zilong <liuzl@suosi.com.cn>
2022-10-14 17:51:49 +0800

    rabbitmq

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/x-funs/go-fun"
 	"jassue-gin/bootstrap"
 	"jassue-gin/global"
 	"jassue-gin/setup"
@@ -43,7 +44,30 @@ func main() {
 
 	//初始化mq
 	setup.Rabbit()
+	if simple, err := bootstrap.NewSimple("comment"); err == nil {
+		for i := 0; i < 100; i++ {
+			msg := Demo{
+				Id:   i + 1,
+				Name: fun.RandomLetter(4),
+				Time: fun.Date(fun.DatetimeMilliPattern),
+			}
+			msgJson := fun.ToJson(msg)
+			if err := simple.Send(fun.Bytes(msgJson)); err == nil {
+				bootstrap.Info("Send simple success", bootstrap.String("msg", fun.ToString(msg)))
+			} else {
+				bootstrap.Error("Send simple error")
+			}
+		}
+	}
+
 	// 启动服务器
 	bootstrap.RunServer()
 
+}
+
+type Demo struct {
+	Id   int    `json:"id"`
+	Name string `json:"name"`
+	Key  string `json:"key"`
+	Time string `json:"time"`
 }
